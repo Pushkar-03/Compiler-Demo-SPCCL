976bf06a7e8414f075e70770e13c2530eb5584d0
Pushkar Hemant Bihani <phbstudy@gmail.com>
2026-05-11 00:08:57 +0530

    Refactor MiniLangCompiler for improved parsing

--- SPCCL JURY/main.py
+++ SPCCL JURY/main.py
@@ -1,28 +1,38 @@
 import re
 
+
 class MiniLangCompiler:
 
     def __init__(self):
 
-        self.variables = {}
         self.tokens = []
         self.errors = []
+        self.variables = {}
+        self.logs = []
+
 
     def lexer(self, code):
 
         token_specification = [
 
             ('NUMBER', r'\d+'),
-            ('KEYWORD', r'LET|IF|THEN|PRINT|END'),
-            ('OPERATOR', r'==|>=|<=|>|<|='),
+            ('STRING', r'"[^"]*"'),
+
+            ('KEYWORD', r'\b(var|if|then|else|end|print)\b'),
+
+            ('OPERATOR', r'==|!=|>=|<=|>|<|='),
+
             ('LPAREN', r'\('),
             ('RPAREN', r'\)'),
+
             ('SEMICOLON', r';'),
+
             ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z0-9_]*'),
 
             ('NEWLINE', r'\r\n|\r|\n'),
 
             ('SKIP', r'[ \t]+'),
+
             ('MISMATCH', r'.')
         ]
 
@@ -59,77 +69,76 @@ class MiniLangCompiler:
                     'line': line_num
                 })
 
+
     def parse(self):
 
         i = 0
-        execution_log = []
 
         while i < len(self.tokens):
 
             token = self.tokens[i]
 
-            if token['value'] == 'LET':
+            if token['value'] == 'var':
 
                 try:
 
                     identifier = self.tokens[i + 1]
-                    operator = self.tokens[i + 2]
+                    equals = self.tokens[i + 2]
                     value = self.tokens[i + 3]
                     semicolon = self.tokens[i + 4]
 
                     if identifier['type'] != 'IDENTIFIER':
+
                         self.errors.append(
                             f"Syntax Error at line {identifier['line']}: Identifier expected"
                         )
 
-                    if operator['value'] != '=':
+                    if equals['value'] != '=':
+
                         self.errors.append(
-                            f"Syntax Error at line {operator['line']}: '=' expected"
+                            f"Syntax Error at line {equals['line']}: '=' expected"
                         )
 
-                    if value['type'] != 'NUMBER':
+                    if value['type'] not in ['NUMBER', 'STRING']:
+
                         self.errors.append(
-                            f"Syntax Error at line {value['line']}: Number expected"
+                            f"Syntax Error at line {value['line']}: Invalid value"
                         )
 
                     if semicolon['type'] != 'SEMICOLON':
+
                         self.errors.append(
                             f"Syntax Error at line {semicolon['line']}: ';' missing"
                         )
 
-                    self.variables[identifier['value']] = int(value['value'])
 
-                    execution_log.append(
-                        f"Variable '{identifier['value']}' assigned value {value['value']}"
+                    if value['type'] == 'NUMBER':
+                        self.variables[identifier['value']] = int(value['value'])
+
+                    else:
+                        self.variables[identifier['value']] = value['value'].replace('"', '')
+
+                    self.logs.append(
+                        f"Variable '{identifier['value']}' declared"
                     )
 
                     i += 5
 
                 except:
+
                     self.errors.append(
-                        "Unexpected end of assignment statement"
+                        "Invalid variable declaration"
                     )
+
                     break
 
 
-            elif token['value'] == 'PRINT':
+            elif token['value'] == 'print':
 
                 try:
 
-                    identifier = self.tokens[i + 1]
+                    value = self.tokens[i + 1]
                     semicolon = self.tokens[i + 2]
-
-                    if identifier['value'] not in self.variables:
-
-                        self.errors.append(
-                            f"Semantic Error at line {identifier['line']}: Variable '{identifier['value']}' not declared"
-                        )
-
-                    else:
-
-                        execution_log.append(
-                            f"OUTPUT => {identifier['value']} = {self.variables[identifier['value']]}"
-                        )
 
                     if semicolon['type'] != 'SEMICOLON':
 
@@ -137,74 +146,137 @@ class MiniLangCompiler:
                             f"Syntax Error at line {semicolon['line']}: ';' missing"
                         )
 
+
+                    if value['type'] == 'IDENTIFIER':
+
+                        if value['value'] not in self.variables:
+
+                            self.errors.append(
+                                f"Semantic Error at line {value['line']}: Variable '{value['value']}' not declared"
+                            )
+
+                        else:
+
+                            output = self.variables[value['value']]
+
+                            self.logs.append(
+                                f"OUTPUT => {output}"
+                            )
+
+
+                    elif value['type'] == 'STRING':
+
+                        output = value['value'].replace('"', '')
+
+                        self.logs.append(
+                            f"OUTPUT => {output}"
+                        )
+
+                    else:
+
+                        self.errors.append(
+                            f"Syntax Error at line {value['line']}: Invalid print value"
+                        )
+
                     i += 3
 
                 except:
-                    self.errors.append("Invalid PRINT statement")
+
+                    self.errors.append(
+                        "Invalid print statement"
+                    )
+
                     break
 
-            # ---------------- IF CONDITION ----------------
-
-            elif token['value'] == 'IF':
+            elif token['value'] == 'if':
 
                 try:
 
-                    lp = self.tokens[i + 1]
-                    left = self.tokens[i + 2]
-                    op = self.tokens[i + 3]
-                    right = self.tokens[i + 4]
-                    rp = self.tokens[i + 5]
-                    then = self.tokens[i + 6]
+                    left = self.tokens[i + 1]
+                    operator = self.tokens[i + 2]
+                    right = self.tokens[i + 3]
+                    then = self.tokens[i + 4]
 
-                    if lp['type'] != 'LPAREN':
-                        self.errors.append("Missing '(' in IF")
+                    if left['value'] not in self.variables:
 
-                    if rp['type'] != 'RPAREN':
-                        self.errors.append("Missing ')' in IF")
+                        self.errors.append(
+                            f"Semantic Error: Variable '{left['value']}' not declared"
+                        )
 
-                    if then['value'] != 'THEN':
-                        self.errors.append("THEN keyword missing")
+                    if then['value'] != 'then':
 
-                    condition_result = False
+                        self.errors.append(
+                            f"Syntax Error at line {then['line']}: 'then' expected"
+                        )
 
-                    left_val = self.variables.get(left['value'], 0)
-                    right_val = int(right['value'])
+                    left_value = self.variables.get(left['value'], 0)
+                    right_value = int(right['value'])
 
-                    if op['value'] == '>':
-                        condition_result = left_val > right_val
+                    condition = False
 
-                    elif op['value'] == '<':
-                        condition_result = left_val < right_val
+                    if operator['value'] == '>':
+                        condition = left_value > right_value
 
-                    execution_log.append(
-                        f"IF Condition Evaluated => {condition_result}"
+                    elif operator['value'] == '<':
+                        condition = left_value < right_value
+
+                    elif operator['value'] == '==':
+                        condition = left_value == right_value
+
+                    self.logs.append(
+                        f"IF condition evaluated to {condition}"
                     )
 
-                    i += 7
+                    i += 5
 
                 except:
-                    self.errors.append("Invalid IF statement")
+
+                    self.errors.append(
+                        "Invalid IF statement"
+                    )
+
                     break
+
+
+            elif token['value'] == 'else':
+
+                self.logs.append("ELSE block detected")
+
+                i += 1
+
+
+            elif token['value'] == 'end':
+
+                self.logs.append("END block detected")
+
+                i += 1
+
 
             else:
 
                 self.errors.append(
-                    f"Unknown token '{token['value']}' at line {token['line']}"
+                    f"Unknown syntax near '{token['value']}' at line {token['line']}"
                 )
 
                 i += 1
 
-        return execution_log
 
     def compile(self, code):
 
+        code = code.replace('\r\n', '\n')
+        code = code.replace('\r', '\n')
+
         self.lexer(code)
 
-        logs = self.parse()
+        self.parse()
 
         return {
+
             "tokens": self.tokens,
+
             "variables": self.variables,
-            "logs": logs,
+
+            "logs": self.logs,
+
             "errors": self.errors
         }
